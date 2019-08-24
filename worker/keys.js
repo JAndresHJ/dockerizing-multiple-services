f38a4223d66a3588a63fb4f00ecba0760aa29b75
Jorge <jandreshdzj@gmail.com>
2019-08-24 11:34:28 -0500

    FIX when building container

--- worker/keys.js
+++ worker/keys.js
@@ -1,8 +1,3 @@
-/**
- * This file contains the hostname and the port required
- * to connect with Reddis
- */
-
 module.exports = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT
